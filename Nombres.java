cbc6ded76819ab1abb312b1380d1e81965b0435e
CódigoTipado <33013656+codigotipado@users.noreply.github.com>
2019-04-14 23:49:58 -0400

    Update Nombres.java
    Uso del metodo equals

--- Nombres.java
+++ Nombres.java
@@ -12,15 +12,6 @@ public class Nombres{
 		System.out.print("Porfavor dame el segundo nombre: ");
 		nombre_dos = entrada.nextLine();
 
-		/*equals : compara que ambos nombres sean exactamente iguales en todo
-		y hace distincion de mayusculas y minusculas
-
-
-		pero si yo quisiera que no realizara esa distincion de mayusculas y minusculas
-		solo que evaluara si son el mismo nombre o no, eso se haria asi: equalsIgnoreCase()
-		*/
-
-
 		if(nombre_uno.equalsIgnoreCase(nombre_dos)){
 			System.out.println("los nombres son iguales");
 		}else{
